fbfe16147704ac10acdb5620a44bb430f0d0f723
Vinícius Vargas Ribeiro <vinicius.vargas@conectt.com.br>
2020-11-05 00:25:02 -0300

    [arrays] Adicionado teste de rotation left

---
+++ arrays/arrays-left-rotation/solution.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the rotLeft function below.
+function rotLeft(a, d) {
+    for(let i=0; i < d; i++){
+        a.push(a.shift(0))
+    }
+    return a
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nd = readLine().split(' ');
+
+    const n = parseInt(nd[0], 10);
+
+    const d = parseInt(nd[1], 10);
+
+    const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = rotLeft(a, d);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
+
+function mainCustom() {
+
+    const d = 4
+
+    const a = '1 2 3 4 5'.split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = rotLeft(a, d);
+
+    console.log(result + "\n");
+}
+
+mainCustom()
